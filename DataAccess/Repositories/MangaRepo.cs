1bb26b63631139b740308931923a846e360100f8
Yaroslav Holota <korova2321@gmail.com>
2021-01-23 12:17:14 +0200

    Created manga repo

---
+++ DataAccess/Repositories/MangaRepo.cs
@@ -0,0 +1,38 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repositories
+{
+    public class MangaRepo : IMangaRepo
+    {
+        private readonly ISQLClient _client;
+
+        public MangaRepo(ISQLClient client)
+        {
+            this._client = client;
+        }
+
+        public async Task<Manga> FindMangaInfoByIDAsync(string mangaId)
+        {
+            string sql = @"SELECT MangaTitle, Description FROM Mangas WHERE Id=@MangaId";
+
+            var parameters = new
+            {
+                MangaId = mangaId 
+            };
+
+            List<Manga> matches = await _client.LoadData<Manga, dynamic>
+                (sql, parameters, CancellationToken.None);
+            if (matches.Count == 0)
+            {
+                //can't find manga
+            }
+            return matches.First();
+        }
+    }
+}
