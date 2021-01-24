44fe87fc0ba8c90a96e521d40a73220d50174eea
Yaroslav Holota <korova2321@gmail.com>
2021-01-24 18:42:06 +0200

    Added seeded data

--- MangaLWebAPI/Program.cs
+++ MangaLWebAPI/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
 
 namespace MangaLWebAPI
 {
@@ -9,9 +11,10 @@ namespace MangaLWebAPI
         public static async Task Main(string[] args)
         {
             var host = CreateHostBuilder(args).Build();
+            await SeedDataIfNecessary(host);
             host.Run();
         }
-
+   
         public static IHostBuilder CreateHostBuilder(string[] args)
         {
             return Host.CreateDefaultBuilder(args)
@@ -20,6 +23,19 @@ namespace MangaLWebAPI
                         webBuilder.UseKestrel();
                         webBuilder.UseStartup<Startup>();
                     });
+        }
+        private static async Task SeedDataIfNecessary(IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var config = scope.ServiceProvider.GetRequiredService<IConfiguration>();
+
+                if (config.GetValue<bool>("SeedData"))
+                {
+                    await Seeder.SeedMangas(host);
+                    await Seeder.SeedUsers(host);
+                }
+            }
         }
     }
 }
--- MangaLWebAPI/Seeder.cs
+++ MangaLWebAPI/Seeder.cs
@@ -1,9 +1,14 @@
 ﻿using Infrastructure.Models;
 using Infrastructure.Services;
+using MangaLWebAPI.Mediatr;
+using MediatR;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -19,11 +24,119 @@ namespace MangaLWebAPI
 
                 var userService = provider.GetRequiredService<IUserServices>();
 
-                UserRegistrationModel model =
-                    new UserRegistrationModel("Olezhka", "oleg123");
+                string userFilePath = Path.Combine(GetSeeededDataDirectory(), "Users.json");
 
-                await userService.RegisterUser(model);
+                using (FileStream file = new FileStream(userFilePath, FileMode.Open, FileAccess.Read))
+                {
+                    using (StreamReader reader = new StreamReader(file))
+                    {
+                        string content = reader.ReadToEnd();
+                        List<UserRegistrationModel> users =
+                            JsonConvert.DeserializeObject<List<UserRegistrationModel>>(content);
+                        foreach (var user in users)
+                        {
+                            await userService.RegisterUser(user);
+                        }
+                    }
+                }
             }
         }
+        private static string GetSeeededDataDirectory()
+        {
+            return Path.Combine(Directory.GetCurrentDirectory(), "SeededData");
+        }
+        public static async Task SeedMangas(IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var provider = scope.ServiceProvider;
+
+                IMediator mediator = provider.GetRequiredService<IMediator>();
+
+                string mangaInfoPath = Path.Combine(GetSeeededDataDirectory(), "Manga.json");
+
+                MangaUploadCommand manga =
+                      JsonConvert.DeserializeObject<MangaUploadCommand>(GetFileContent(mangaInfoPath));
+
+                string mangaId = await mediator.Send(manga);
+
+                string chaptersInfoPath = Path.Combine(GetSeeededDataDirectory(), "Chapters.json");
+
+                List<ChapterUploadCommand> chapters =
+                    JsonConvert.DeserializeObject<List<ChapterUploadCommand>>(GetFileContent(chaptersInfoPath));
+                List<string> ChapterIds = new List<string>();
+                foreach (var chapter in chapters)
+                {
+                    chapter.MangaId = mangaId;
+                    string id = await mediator.Send(chapter);
+                    ChapterIds.Add(id);
+                }
+
+                string picturesInfoPath = Path.Combine(GetSeeededDataDirectory(), "Pictures.json");
+
+                List<PictureIntermediary> pictures =
+                    JsonConvert.DeserializeObject<List<PictureIntermediary>>(GetFileContent(picturesInfoPath));
+
+                Dictionary<string, string> ChapterNumberToChapterId = new Dictionary<string, string>();
+
+                var picturesWithUniqueChapter = pictures
+                        .GroupBy(x => x.Chapter)
+                        .Select(x => x.First())
+                        .ToList();
+
+                for (int i = 0; i < picturesWithUniqueChapter.Count; i++)
+                {
+                    ChapterNumberToChapterId.TryAdd(picturesWithUniqueChapter[i].Chapter, ChapterIds[i]);
+                }
+
+                string pictureLocationBase = Path.Combine(GetSeeededDataDirectory(), "Pictures");
+
+                Dictionary<PictureIntermediary, IFormFile> pictureToFile =
+                    new Dictionary<PictureIntermediary, IFormFile>();
+
+                foreach (var picture in pictures)
+                {
+                    string path = Path.Combine(pictureLocationBase, picture.Chapter, picture.Order);
+
+                    path = Path.ChangeExtension(path, "jpg");
+
+                    FileStream file = new FileStream(path, FileMode.Open, FileAccess.Read);
+
+                    IFormFile formFile = new FormFile(file, 0, file.Length,
+                        "Alexander", "Oleg");
+                    pictureToFile.Add(picture, formFile);
+                }
+
+                List<ImageUploadCommand> images = new List<ImageUploadCommand>();
+
+                foreach (var picture in pictures)
+                {
+                    var form = pictureToFile.GetValueOrDefault(picture);
+
+                    int order = int.Parse(picture.Order);
+
+                    string ChapterId = ChapterNumberToChapterId.GetValueOrDefault(picture.Chapter);
+
+                    images.Add(new ImageUploadCommand(form, ChapterId, order));
+                }
+
+                foreach (var image in images)
+                {
+                    await mediator.Send(image);
+                }
+            }
+        }
+        private static string GetFileContent(string path)
+        {
+            using (FileStream file = new FileStream(path, FileMode.Open, FileAccess.Read))
+            {
+                using (StreamReader reader = new StreamReader(file))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+        }
+
+        private record PictureIntermediary(string Chapter, string Order);
     }
 }
