d8d4188534a79f82ebf365856c37412cf24bcbbb
Yaroslav Holota <korova2321@gmail.com>
2021-01-24 12:04:52 +0200

    Updated model to match its purpose

--- Core/Entities/Picture.cs
+++ Core/Entities/Picture.cs
@@ -4,7 +4,7 @@
     {
         public string Id { get; set; }
         public int PictureOrder { get; set; }
-        public string MangaId { get; set; }
+        public string ChapterId { get; set; }
         public Manga Manga { get; set; }
         public string ImageLocation { get; set; }
     }
