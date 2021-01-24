7d150f21566a8221073c2aece6b1f2b11580e59f
Yaroslav Holota <korova2321@gmail.com>
2021-01-24 12:42:09 +0200

    Moved info models into a separate folder

---
+++ DataAccess/DTOs/Info/PictureInfoModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.DTOs
+{
+    public class PictureInfoModel
+    {
+        public string ChapterId { get; set; }
+        public string PictureLocation { get; set; }
+        public int PictureOrder { get; set; }
+    }
+}
