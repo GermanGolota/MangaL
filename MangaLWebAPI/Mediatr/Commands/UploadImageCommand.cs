5c6bae495cda1a457b9e6fec14c73673c27fe253
Yaroslav Holota <korova2321@gmail.com>
2021-01-24 17:17:33 +0200

    Implemented upload image using mediatr

---
+++ MangaLWebAPI/Mediatr/Commands/UploadImageCommand.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MangaLWebAPI.Mediatr
+{
+    public class UploadImageCommand:IRequest<string>
+    {
+        public string ChapterId { get; set; }
+        public int Order { get; set; }
+        public IFormFile File { get; set; }
+        public UploadImageCommand(IFormFile file, string chapterId, int order)
+        {
+            File = file;
+            ChapterId = chapterId;
+            Order = order;
+        }
+    }
+}
---
+++ MangaLWebAPI/Mediatr/Handlers/UploadImageHandler.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using DataAccess.DTOs;
+using DataAccess.Repositories;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+
+namespace MangaLWebAPI.Mediatr
+{
+    public class UploadImageHandler : IRequestHandler<UploadImageCommand, string>
+    {
+        private readonly IMangaWriteRepo _repo;
+        private readonly IMangaReadRepo _readRepo;
+
+        public UploadImageHandler(IMangaWriteRepo repo, IMangaReadRepo readRepo)
+        {
+            this._repo = repo;
+            this._readRepo = readRepo;
+        }
+        public async Task<string> Handle(UploadImageCommand request, CancellationToken cancellationToken)
+        {
+            var file = request.File;
+            string chapterId = request.ChapterId;
+
+            if (isNotValidFile(file))
+            {
+            }
+
+            var picture = new PictureAdditionModel
+            {
+                ChapterId = chapterId,
+                PictureOrder = request.Order
+            };
+
+            string imageId = await _repo.SavePictureReturnId(picture, cancellationToken);
+
+            string mangaId = await _readRepo.FindMangaIdForChapter(chapterId, cancellationToken);
+
+            string path = GetFilePath(file, imageId, chapterId, mangaId);
+
+            using (FileStream fs = System.IO.File.Create(path))
+            {
+                file.CopyTo(fs);
+                fs.Flush();
+            }
+
+            await _repo.UpdatePictureLocation(imageId, path, cancellationToken);
+
+            return imageId;
+        }
+        private string GetFilePath(IFormFile file, string imageId, string chapterId, string mangaId)
+        {
+            string fileName = Path.GetFileName(file.FileName);
+            string fileExtension = Path.GetExtension(fileName);
+
+            var newFileName = String.Concat(imageId, fileExtension);
+
+            string rootFolder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot");
+
+            string mangaFolder = Path.Combine(rootFolder, "Mangas", $"{mangaId}");
+
+            string chaptersFolder = Path.Combine(mangaFolder, "Chapters", $"{chapterId}");
+
+            string imageFolderPath = Path.Combine(chaptersFolder, "Images");
+
+            Directory.CreateDirectory(imageFolderPath);
+
+            string filePath = Path.Combine(imageFolderPath, $"{newFileName}");
+
+            return filePath;
+        }
+        private bool isNotValidFile(IFormFile file)
+        {
+            return file == null || file.Length == 0;
+        }
+    }
+}
