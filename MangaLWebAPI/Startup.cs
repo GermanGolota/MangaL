62cd25ad0705011253e3e5cd200623e547736187
Yaroslav Holota <korova2321@gmail.com>
2021-01-24 13:48:19 +0200

    Added swagger

--- MangaLWebAPI/Startup.cs
+++ MangaLWebAPI/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -38,10 +39,19 @@ namespace MangaLWebAPI
             services.AddRepositories();
 
             services.AddInfrastructureServices();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "MangaLAPI", Version = "v1" });
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MangaL API v1"));
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
