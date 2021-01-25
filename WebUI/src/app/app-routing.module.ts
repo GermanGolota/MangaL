b48977db27507898f0f2e98f1ee77a64b12ce7e4
Yaroslav Holota <korova2321@gmail.com>
2021-01-25 21:03:04 +0200

    Added angular project for ui

---
+++ WebUI/src/app/app-routing.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { ɵangular_packages_platform_browser_platform_browser_a } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
