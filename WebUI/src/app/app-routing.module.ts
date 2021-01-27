5034d8c43f5cdd32f0c20d7a149b1a63f6d6c1f7
Yaroslav Holota <korova2321@gmail.com>
2021-01-27 22:55:57 +0200

    Updated application component to use navbar

--- WebUI/src/app/app-routing.module.ts
+++ WebUI/src/app/app-routing.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { ɵangular_packages_platform_browser_platform_browser_a } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [];
