935fce9b8fa336620797f8c2e0d08c1b9bd9bfee
Yaroslav Holota <korova2321@gmail.com>
2021-01-29 23:09:17 +0200

    Added routing

--- WebUI/src/app/app-routing.module.ts
+++ WebUI/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChapterComponent } from './manga/components/chapter/chapter.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { RandomMangaComponent } from './manga/components/random-manga/random-manga.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component: LandingPageComponent
+  },
+  {
+    path:'random',
+    component: RandomMangaComponent
+  },
+  {
+    path:'chapter/:chapterId',
+    component: ChapterComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
