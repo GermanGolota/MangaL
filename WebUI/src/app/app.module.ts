b3ed0152a94e117413ca312065334a75b9e62669
Yaroslav Holota <korova2321@gmail.com>
2021-01-29 23:10:41 +0200

    Used routing in the app component

--- WebUI/src/app/app.module.ts
+++ WebUI/src/app/app.module.ts
@@ -1,19 +1,25 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { MangaModule } from './manga/manga.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CoreModule
+    CoreModule,
+    HttpClientModule,
+    MangaModule
   ],
-  providers: [],
+  providers: [HttpClient],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
